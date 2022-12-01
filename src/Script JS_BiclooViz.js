cd6d1869cfe2812209146d0ce549166eb20c49a7
Corentin MONVILLERS <c.monvillers@live.fr>
2022-12-01 15:15:14 +0100

    new version fecth and first chart

--- src/Script JS_BiclooViz.js
+++ src/Script JS_BiclooViz.js
@@ -1,17 +1,53 @@
-async function getTotalBikes(){
-return fetch('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&rows=200&facet=banking&facet=bonus&facet=status&facet=contract_name')
-  .then((response) => response.json())
-  .then((data) => {
-   // console.log(data.records[0].fields.available_bike_stands)
-   // console.log(data)
-    let bikeSum = 0 
-    for (let i = 0; i < data.records.length; i++){
-        bikeSum +=  data.records[i].fields.available_bike_stands
-        }
-    return bikeSum
-  })
+let url =
+  "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_stations-velos-libre-service-nantes-metropole-disponibilites&q=&rows=200&facet=banking&facet=bonus&facet=status&facet=contract_name";
 
+//Récupèrer l'ensemble de la data et de la retranscrire en JS
+async function getBikes() {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
 }
 
-let resultCirculatingBikes = getTotalBikes()
-.then((circulatingBikes) => console.log(circulatingBikes))
+//
+async function totalBikes() {
+//Permet de stocker la data en JS fournie par la fonction getBikes()
+  let resultBikes = await getBikes(url);
+//Création des variables d'initialisations pour nos différents calculs
+  let bikeSum = 0;
+  let totalBike = 0;
+
+  //Boucle pour avoir les vélos en circulation
+  for (let i = 0; i < resultBikes.records.length; i++) {
+    bikeSum += resultBikes.records[i].fields.available_bike_stands;
+  }
+  console.log(bikeSum);
+
+  //Boucle pour avoir le total de vélos 
+  for (let i = 0; i < resultBikes.records.length; i++) {
+    totalBike += resultBikes.records[i].fields.bike_stands;
+  }
+
+  let ctx = document.getElementById("graph1");
+
+  //Conversion du nombre de vélos en circulation en pourcentage
+  let result = Math.round((bikeSum * 100) / totalBike);
+
+  //Initialisation du nouveau graphique
+  new Chart(ctx, {
+
+    type: "doughnut",
+    data: {
+      labels: ["Velos en circulation", "Total de velos"],
+      datasets: [
+        {
+          labels: "# de velos",
+          backgroundColor: ["#000192", "#000000"],
+          data: [result, (100 - result)],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {},
+  });
+}
+totalBikes();
